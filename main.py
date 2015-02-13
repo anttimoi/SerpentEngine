8adcaa0432e0ddb28193a3297ef0c2e60e22b4ad
Antti Kallonen <kallonen.antti@gmail.com>
2015-02-13 19:05:05 +0200

    Collision system

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame, random, math, sys
 
 tileSize = 32
-screenResolution = [1280, 720]
+screenResolution = [800,600]
 tileSetWidth = 0
 tileSetHeight = 0
 fps = 60
@@ -17,21 +17,30 @@ class Character:
     __metaclass__ = IterRegistry
     _registry = []
 
-    def __init__(self, name):
+    def __init__(self, name, map):
         self._registry.append(self)
         self.name = name
         self.x = 0
         self.y = 0
         self.speed = 3
         self.sprite = pygame.Surface((tileSize, tileSize))
+        self.map = map
 
     def move(self, x, y):
         if x != 0 and y != 0:
             x = (1/math.sqrt(2))*x
             y = (1/math.sqrt(2))*y
 
-        self.x = min((mapSize[0]-1)*tileSize, max(0, self.x + self.speed*x))
-        self.y = min((mapSize[1]-1)*tileSize, max(0, self.y + self.speed*y))
+        newX = min((mapSize[0]-1)*tileSize, max(0, self.x + self.speed*x))
+        newY = min((mapSize[1]-1)*tileSize, max(0, self.y + self.speed*y))
+
+        hit = self.map.isHit(newX,self.y)
+        if hit == False:
+            self.x = newX
+
+        hit = self.map.isHit(self.x,newY)
+        if hit == False:
+            self.y = newY
 
     def place(self, x, y):
         self.x = min((mapSize[0]-1)*tileSize, max(0, x))
@@ -48,6 +57,8 @@ class Map:
     global tileSize
     def __init__(self, size, tileSet):
         self.map = []
+        self.hit = []
+        self.tileSet = tileSet
         self.mapSurface = pygame.Surface((size[0]*tileSize,size[1]*tileSize))
         self.size = size
         self.tileSetWidth = 0
@@ -55,11 +66,17 @@ class Map:
 
     def generateMap(self, tileSet):
         self.map = []
+        self.hit = []
         for y in range(0,self.size[1]):
             slice = []
+            sliceHit = []
             for x in range(0,self.size[0]):
-                slice.append(random.randint(21,25))
+                block = (random.randint(1,10) == 1)*2+1
+                slice.append(block)
+                hit = (slice[x] == 3)
+                sliceHit.append(hit)
             self.map.append(slice)
+            self.hit.append(sliceHit)
         self.mapSurface.set_colorkey((0, 0, 0))
         tileSetFile = pygame.image.load(tileSet)
         self.tileSetWidth = tileSetFile.get_width()
@@ -69,6 +86,21 @@ class Map:
                 tile = pygame.Surface((tileSize,tileSize))
                 tile.blit(tileSetFile, pygame.Rect(-1*self.getTileCoordinates(self.map[y][x])[0],-1*self.getTileCoordinates(self.map[y][x])[1],tileSize,tileSize))
                 self.mapSurface.blit(tile, pygame.Rect(x*tileSize, y*tileSize, tileSize, tileSize))
+
+    def isHit(self, x, y):
+        hitBoxSize = 0.9
+        hit = False
+        points = []
+        for xx in range(0,2):
+            for yy in range(0,2):
+                point = [math.floor((x+tileSize*(hitBoxSize*xx))/tileSize), math.floor((y+tileSize*(hitBoxSize*yy))/tileSize)]
+                points.append(point)
+
+        for point in points:
+            if self.hit[point[1]][point[0]]:
+                hit = True
+
+        return hit
 
     def getTileCoordinates(self, id):
         global tileSize
@@ -115,34 +147,43 @@ class Camera:
         for sprite in renderSprites:
             screen.blit(sprite[0],(sprite[1]+self.offset[0],sprite[2]+self.offset[1]))
 
+    def switch(self, old, new):
+        self.target = new.target
+        self.dampening = new.dampening
+        self.spacing = new.spacing
+        self.offset = old.offset
+
 def main():
-    global tileSize, tileSetWidth, tileSetHeight
+    global tileSize, tileSetWidth, tileSetHeight, screenResolution
 
     pygame.init()
-
+    displayInfo = pygame.display.Info()
+    #screenResolution = [displayInfo.current_w, displayInfo.current_h]
     size = screenResolution[0], screenResolution[1]
-    screen = pygame.display.set_mode(size, pygame.DOUBLEBUF)
+    screen = pygame.display.set_mode(size)
 
-    map = Map(mapSize, "tileSet2.png")
+    map = Map(mapSize, "tileSet.png")
 
     charSprites = pygame.image.load("characters.png")
     sprite = pygame.Surface((tileSize, tileSize))
     sprite.blit(charSprites, pygame.Rect(-32, -32, tileSize, tileSize))
 
-    player = Character("player")
+    player = Character("player", map)
     player.setSprite(sprite)
 
     camera = Camera(player, 10, 20)
+    currentCamera = camera
 
     enemy = [None]*200
 
     for char in enemy:
         sprite = pygame.Surface((tileSize, tileSize))
         sprite.blit(charSprites, pygame.Rect(-1*random.randint(0,5)*32, -1*random.randint(0,5)*32, tileSize, tileSize))
-        char = Character("enemy")
+        char = Character("enemy", map)
         char.setSprite(sprite)
         char.place(random.randint(0,mapSize[0]*tileSize), random.randint(0,mapSize[1]*tileSize))
-
+        enemyTrack = char
+    mapGenFlag = pygame.time.get_ticks()
     while 1:
         frameStart = pygame.time.get_ticks()
 
@@ -155,8 +196,22 @@ def main():
         if keys[pygame.K_ESCAPE]:
             sys.exit()
 
+        for char in Character._registry:
+            if char.name != "player":
+                char.move(random.randint(-1,1), random.randint(-1,1))
+
         # Rendering
-        camera.render(screen, map, Character._registry)
+        if keys[pygame.K_1] and currentCamera.target != player:
+            currentCamera.target = player
+            print("Switching to player")
+        if keys[pygame.K_2] and currentCamera.target != enemyTrack:
+            currentCamera.target = enemyTrack
+            print("Switching to enemy")
+        if keys[pygame.K_3] and pygame.time.get_ticks()-mapGenFlag>500:
+            mapGenFlag = pygame.time.get_ticks()
+            map.generateMap(map.tileSet)
+
+        currentCamera.render(screen, map, Character._registry)
 
         pygame.display.flip()
 
